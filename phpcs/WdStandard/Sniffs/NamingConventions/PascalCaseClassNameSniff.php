1caa2a729c66cb351635d9bd2ccf9b2e80c81588
Martin Holler <holler@parkside.at>
2019-04-18 10:55:15 +0200

    Add sniff for class lines of code

--- phpcs/WdStandard/Sniffs/NamingConventions/PascalCaseClassNameSniff.php
+++ phpcs/WdStandard/Sniffs/NamingConventions/PascalCaseClassNameSniff.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Tests that classes are named in PascalCase.
+ * Ensures that classes are named in PascalCase.
  */
 class WdStandard_Sniffs_NamingConventions_PascalCaseClassNameSniff implements PHP_CodeSniffer_Sniff
 {
@@ -10,7 +10,10 @@ class WdStandard_Sniffs_NamingConventions_PascalCaseClassNameSniff implements PH
      */
     public function register()
     {
-        return [T_CLASS, T_INTERFACE];
+        return [
+            T_CLASS,
+            T_INTERFACE,
+        ];
     }
 
     /**
@@ -19,11 +22,10 @@ class WdStandard_Sniffs_NamingConventions_PascalCaseClassNameSniff implements PH
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
         $tokens = $phpcsFile->getTokens();
-        $tokenPosition = $phpcsFile->findNext(T_STRING, $stackPtr);
-        $token = $tokens[$tokenPosition];
+        $tokenClassName = $tokens[$phpcsFile->findNext(T_STRING, $stackPtr)];
 
-        if (!preg_match('/^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/', $token['content'])) {
-            $phpcsFile->addError("Class name {$token['content']} is not PascalCase", $stackPtr, 'NotPascalCase');
+        if (!preg_match('/^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/', $tokenClassName['content'])) {
+            $phpcsFile->addError("Class name {$tokenClassName['content']} is not PascalCase", $stackPtr, 'NotPascalCase');
         }
     }
 }
