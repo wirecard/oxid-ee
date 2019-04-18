be173ba17e4d990f40fa1899d7750c75d53496cb
Martin Holler <holler@parkside.at>
2019-04-18 15:20:02 +0200

    Add custom standard & new sniffs

---
+++ phpcs/Custom/Sniffs/Classes/ClassLinesOfCodeSniff.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Ensures that classes do not exceed a certain amount of lines of code.
+ */
+class Custom_Sniffs_Classes_ClassLinesOfCodeSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * The limit of lines of code a class should not exceed.
+     *
+     * @var int
+     */
+    public $linesLimit = 400;
+
+    /**
+     * The limit of lines of code a class must not exceed.
+     *
+     * @var int
+     */
+    public $absoluteLinesLimit = 500;
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [
+            T_CLASS,
+            T_INTERFACE,
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $tokenClass = $tokens[$stackPtr];
+        $tokenScopeOpener = $tokens[$tokenClass['scope_opener']];
+        $tokenScopeCloser = $tokens[$tokenClass['scope_closer']];
+        $linesOfCode = $tokenScopeCloser['line'] - $tokenScopeOpener['line'] - 1;
+
+        if ($linesOfCode > $this->absoluteLinesLimit) {
+            $phpcsFile->addError(
+                'Class exceeds maximum limit of %s lines of code; contains %s lines',
+                $stackPtr,
+                'MaxExceeded',
+                [
+                    $this->absoluteLinesLimit,
+                    $linesOfCode,
+                ]
+            );
+        } else if ($linesOfCode > $this->linesLimit) {
+            $phpcsFile->addWarning(
+                'Class exceeds %s lines of code; contains %s lines',
+                $stackPtr,
+                'TooLong',
+                [
+                    $this->linesLimit,
+                    $linesOfCode,
+                ]
+            );
+        }
+    }
+}
---
+++ phpcs/Custom/Sniffs/NamingConventions/PascalCaseClassNameSniff.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Ensures that classes are named in PascalCase.
+ */
+class Custom_Sniffs_NamingConventions_PascalCaseClassNameSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [
+            T_CLASS,
+            T_INTERFACE,
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $tokenClassName = $tokens[$phpcsFile->findNext(T_STRING, $stackPtr)];
+
+        if (!preg_match('/^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/', $tokenClassName['content'])) {
+            $phpcsFile->addError(
+                'Class name %s is not PascalCase',
+                $stackPtr,
+                'NotPascalCase',
+                [$tokenClassName['content']]
+            );
+        }
+    }
+}
