d6f7b1d6e4cbf6b692c361e8c0b772381cef698d
Martin Holler <holler@parkside.at>
2019-04-24 12:36:48 +0200

    Refactor function name underscore sniff

--- phpcs/Custom/Sniffs/NamingConventions/FunctionNameUnderscoreSniff.php
+++ phpcs/Custom/Sniffs/NamingConventions/FunctionNameUnderscoreSniff.php
@@ -2,69 +2,72 @@
 
 /**
  * Ensures that all private and protected methods start with an underscore and public methods do not.
- * The '__construct' method is excluded from this check,
  */
 class Custom_Sniffs_NamingConventions_FunctionNameUnderscoreSniff implements PHP_CodeSniffer_Sniff
 {
     /**
-     * Returns the token types that this sniff is interested in.
-     * @return array
+     * @inheritdoc
      */
-    public function register(): array
+    public function register()
     {
         return [T_FUNCTION];
     }
 
     /**
-     * Processes the tokens that this sniff is interested in.
-     *
-     * @param PHP_CodeSniffer_File $file     The file where the token was found.
-     * @param integer              $position The position in the stack where the token was found.
+     * @inheritdoc
      */
-    public function process(PHP_CodeSniffer_File $file, $position)
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
-        // gets the function name (is the next string token after the function token)
-        $functionNamePosition = $file->findNext([T_STRING], $position);
-        $functionNameToken = $file->getTokens()[$functionNamePosition];
-        $functionName = $functionNameToken['content'];
+        $tokens = $phpcsFile->getTokens();
+        $tokenFunctionNamePosition = $phpcsFile->findNext(T_STRING, $stackPtr);
+        $tokenFunctionName = $tokens[$tokenFunctionNamePosition];
+        $tokenVisibilityPosition = $phpcsFile->findPrevious([T_PRIVATE, T_PROTECTED, T_PUBLIC], $stackPtr);
+        $tokenVisibility = $tokens[$tokenVisibilityPosition];
 
-        // gets the visibility keyword previous from the function token
-        $visibilityKeywordPosition = $file->findPrevious([T_PRIVATE, T_PROTECTED, T_PUBLIC], $position);
-        $visibilityKeywordToken = $file->getTokens()[$visibilityKeywordPosition];
-        $visibilityKeyword = $visibilityKeywordToken['content'];
+        switch ($tokenVisibility['code']) {
+            case T_PRIVATE:
+                if ($tokenFunctionName['content'][0] !== '_') {
+                    $fix = $phpcsFile->addFixableError(
+                        'Private functions must start with an underscore',
+                        $tokenFunctionNamePosition,
+                        'PrivateFunctionNameUnderscore'
+                    );
 
-        switch ($visibilityKeyword) {
-            case 'private':
-                // private methods MUST start with an underscore
-                if (strpos($functionName, '_') !== 0) {
-                    $errorMsg = 'Private functions must start with an underscore';
-                    $file->addFixableError($errorMsg, $position + 2, __CLASS__);
-                    $file->fixer->addContentBefore($position + 2, '_');
+                    if ($fix) {
+                        $phpcsFile->fixer->addContentBefore($tokenFunctionNamePosition, '_');
+                    }
                 }
 
                 break;
+            case T_PROTECTED:
+                if ($tokenFunctionName['content'][0] !== '_') {
+                    $fix = $phpcsFile->addFixableError(
+                        'Protected functions must start with an underscore',
+                        $tokenFunctionNamePosition,
+                        'ProtectedFunctionNameUnderscore'
+                    );
 
-            case 'protected':
-                // protected methods MUST start with an underscore
-                if (strpos($functionName, '_') !== 0) {
-                    $errorMsg = 'Protected functions must start with an underscore';
-                    $file->addFixableError($errorMsg, $position + 2, __CLASS__);
-                    $file->fixer->substrToken($position + 2, 1);
+                    if ($fix) {
+                        $phpcsFile->fixer->addContentBefore($tokenFunctionNamePosition, '_');
+                    }
                 }
 
                 break;
+            case T_PUBLIC:
+                // this check excludes magic methods (e.g. __construct)
+                if ($tokenFunctionName['content'][0] === '_' && $tokenFunctionName['content'][1] !== '_') {
+                    $fix = $phpcsFile->addFixableError(
+                        'Public functions must not start with an underscore',
+                        $tokenFunctionNamePosition,
+                        'PublicFunctionNameUnderscore'
+                    );
 
-            case 'public':
-                // public methods MUST NOT start with an underscore (except for the '__construct' method)
-                if (strpos($functionName, '_') === 0 && $functionName !== '__construct') {
-                    $errorMsg = 'Public functions must NOT start with an underscore';
-                    $file->addFixableError($errorMsg, $position + 2, __CLASS__);
-                    $file->fixer->substrToken($position + 2, 1);
+                    if ($fix) {
+                        $phpcsFile->fixer->substrToken($tokenFunctionNamePosition, 1);
+                    }
                 }
 
                 break;
-
-            default:
         }
     }
 }
