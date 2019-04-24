12e02696e8d5c06a3b98671a4c9dbfe1e3eeca28
Martin Holler <holler@parkside.at>
2019-04-24 16:27:15 +0200

    Add hungarian notation variable name & use statement group sniff

---
+++ phpcs/Custom/Sniffs/Formatting/UseStatementGroupSniff.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Ensures that use statements are grouped.
+ */
+class Custom_Sniffs_Formatting_UseStatementGroupSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [T_USE];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $previousUseTokenPosition = $phpcsFile->findPrevious(T_USE, $stackPtr - 1);
+        $nextUseTokenPosition = $phpcsFile->findNext(T_USE, $stackPtr + 1);
+
+        if (!$previousUseTokenPosition || !$nextUseTokenPosition) {
+            return;
+        }
+
+        $baseNameToken = $tokens[$phpcsFile->findNext(T_STRING, $stackPtr)];
+        $previousBaseNameToken = $tokens[$phpcsFile->findNext(T_STRING, $previousUseTokenPosition)];
+        $nextBaseNameToken = $tokens[$phpcsFile->findNext(T_STRING, $nextUseTokenPosition)];
+
+        // if there are two or more use statements with the same base name, enforce a newline before the next one
+        if ($previousBaseNameToken['content'] === $baseNameToken['content'] &&
+            $nextBaseNameToken['content'] !== $baseNameToken['content'] &&
+            $tokens[$nextUseTokenPosition - 2]['code'] !== T_WHITESPACE) {
+            $fix = $phpcsFile->addFixableWarning('Use statements with equal base names should be grouped', $nextUseTokenPosition);
+
+            if ($fix) {
+                $phpcsFile->fixer->addContentBefore($nextUseTokenPosition, PHP_EOL);
+            }
+        }
+    }
+}
