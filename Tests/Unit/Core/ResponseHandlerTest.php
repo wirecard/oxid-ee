5bdc411475be6b7fb27c681c4ae8b9c91eac2133
Christian Grach <grach@parkside.at>
2019-04-26 10:53:43 +0200

    more tests

---
+++ Tests/Unit/Core/ResponseHandlerTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/oxid-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/oxid-ee/blob/master/LICENSE
+ */
+
+use OxidEsales\Eshop\Application\Model\Order;
+use Wirecard\Oxid\Core\ResponseHandler;
+use Wirecard\PaymentSdk\BackendService;
+use Wirecard\PaymentSdk\Response\SuccessResponse;
+
+class ResponseHandlerTest extends \Wirecard\Test\WdUnitTestCase
+{
+
+    public function testOnSuccessResponse()
+    {
+
+        $oResponseStub = $this->getMockBuilder(SuccessResponse::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $oResponseStub->method('getParentTransactionId')
+            ->willReturn('');
+
+        $oBackendService = $this->getMockBuilder(BackendService::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $oOrder = $this->getMockBuilder(Order::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        try {
+            ResponseHandler::onSuccessResponse($oResponseStub, $oBackendService, $oOrder);
+        } catch (Exception $exc) {
+
+            $this->fail("testOnSuccessResponse failed because " . get_class($exc) . " was thrown");
+        }
+    }
+}
