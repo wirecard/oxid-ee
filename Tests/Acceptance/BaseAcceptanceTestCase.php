77550a149f11d468f7ea812150f9db1b4dbbbf58
Martin Holler <holler@parkside.at>
2019-05-14 14:46:09 +0200

    Add base acceptance test case

--- Tests/Acceptance/BaseAcceptanceTestCase.php
+++ Tests/Acceptance/BaseAcceptanceTestCase.php
@@ -6,23 +6,26 @@
  * - License can be found under:
  * https://github.com/wirecard/oxid-ee/blob/master/LICENSE
  */
-
+ 
 namespace Wirecard\Oxid\Tests\Acceptance;
 
 abstract class BaseAcceptanceTestCase extends \OxidEsales\TestingLibrary\AcceptanceTestCase
 {
-    /** @var int How much time to wait for pages to load. Wait time is multiplied by this value. */
-    protected $_iWaitTimeMultiplier = 3;
-
-    /** @var int How many times to retry after server error. */
-    protected $retryTimes = 1;
-
+    /**
+     * @inheritdoc
+     */
     protected function setUp()
     {
         parent::setUp();
 
-        $this->clearCache();
-        $this->clearCookies();
-        $this->clearTemp();
+        $this->activateTheme('azure');
+    }
+
+    /**
+     * Do not fail tests on log messages.
+     * @inheritdoc
+     */
+    protected function failOnLoggedExceptions()
+    {
     }
 }
