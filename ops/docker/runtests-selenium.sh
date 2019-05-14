875526b97cbf8c985d4ee18ecd757a1a05f902d7
Martin Holler <holler@parkside.at>
2019-05-14 13:20:28 +0200

    Fix module activation error on falsy test assertions

--- ops/docker/runtests-selenium.sh
+++ ops/docker/runtests-selenium.sh
@@ -5,13 +5,11 @@ set -ex
 # run tests
 cd /var/www/html
 
-# We're disabling the install/restore functionality of this OXID script because
-# of path resolution issues during runtime. We invoke reset-shop.db before
-# running Selenium tests.
+# We're disabling the install functionality of this OXID script because of path
+# resolution issues during runtime. We invoke reset-shop.db before running
+# Selenium tests.
 INSTALL_SHOP=0 \
-RESTORE_AFTER_ACCEPTANCE_TESTS=0 \
-RESTORE_SHOP_AFTER_TESTS_SUITE=0 \
-ACTIVATE_ALL_MODULES=1 \
+RETRY_TIMES_AFTER_TEST_FAIL=0 \
 SELENIUM_SERVER_IP=selenium \
 SHOP_URL=http://${OXID_SERVICE} \
     vendor/bin/runtests-selenium
