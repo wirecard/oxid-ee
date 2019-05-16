41769a315f671bda6ada752002f847d53cdbd15a
Martin Holler <holler@parkside.at>
2019-05-16 16:16:38 +0200

    Add sofort checkout test

---
+++ Tests/Acceptance/SofortCheckoutTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/oxid-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/oxid-ee/blob/master/LICENSE
+ */
+
+namespace Wirecard\Oxid\Tests\Acceptance;
+
+use Wirecard\Oxid\Model\SofortPaymentMethod;
+
+/**
+ * Acceptance tests for the Sofort. checkout flow.
+ */
+class SofortCheckoutTest extends CheckoutTestCase
+{
+    public function getPaymentMethod()
+    {
+        return new SofortPaymentMethod();
+    }
+
+    public function testCheckout()
+    {
+        $this->goThroughCheckout();
+
+        $this->assertPaymentSuccessful();
+    }
+
+    private function goThroughCheckout()
+    {
+        $this->openShop();
+        $this->loginMockUserToFrontend();
+        $this->addMockArticleToBasket();
+
+        // Step 1
+        $this->continueToNextStep();
+
+        // Step 2
+        $this->continueToNextStep();
+
+        // Step 3
+        $this->click($this->getLocator('checkout.paymentMethods.sofort'));
+        $this->continueToNextStep();
+
+        // Step 4
+        $this->continueToNextStep();
+
+        // Sofort. flow
+        $this->select(
+            $this->getLocator('external.sofort.country'),
+            $this->getConfigValue('payments.sofort.country')
+        );
+        $this->type(
+            $this->getLocator('external.sofort.bank'),
+            $this->getConfigValue('payments.sofort.bank')
+        );
+        $this->keyUp($this->getLocator('external.sofort.bank'), ' '); // forces validation
+        $this->clickAndWait($this->getLocator('external.sofort.nextStep'));
+        $this->type(
+            $this->getLocator('external.sofort.userId'),
+            $this->getConfigValue('payments.sofort.userId')
+        );
+        $this->type(
+            $this->getLocator('external.sofort.password'),
+            $this->getConfigValue('payments.sofort.password')
+        );
+        $this->clickAndWait($this->getLocator('external.sofort.nextStep'));
+        $this->click($this->getLocator('external.sofort.account'));
+        $this->clickAndWait($this->getLocator('external.sofort.nextStep'));
+        $this->type(
+            $this->getLocator('external.sofort.tan'),
+            $this->getConfigValue('payments.sofort.tan')
+        );
+        $this->clickAndWait($this->getLocator('external.sofort.nextStep'));
+
+        // Redirect
+        $this->waitForRedirectConfirmation();
+    }
+}
