88fa2bc3fad90a40af7f30c1a70928669d665049
Christian Grach <grach@parkside.at>
2019-05-22 20:12:07 +0200

    improve AccountHolderHelperTest

--- Tests/Unit/Core/AccountHolderHelperTest.php
+++ Tests/Unit/Core/AccountHolderHelperTest.php
@@ -23,9 +23,6 @@ class AccountHolderHelperTest extends OxidEsales\TestingLibrary\UnitTestCase
 
     }
 
-    /**
-     * @throws Exception
-     */
     public function testCreateAccountHolderWithAllDataSet()
     {
         $aArgs = [
@@ -45,19 +42,23 @@ class AccountHolderHelperTest extends OxidEsales\TestingLibrary\UnitTestCase
         $oAccountHolder = $this->_oAccountHolderHelper->createAccountHolder($aArgs);
         $aResult = $oAccountHolder->mappedProperties();
 
-        $this->assertCount(7, $aResult);
-        $this->assertCount(5, $aResult['address']);
-        $this->assertEquals('LastName', $aResult['last-name']);
-        $this->assertEquals('FirstName', $aResult['first-name']);
-        $this->assertEquals('test@test.com', $aResult['email']);
-        $this->assertEquals('02-09-1979', $aResult['date-of-birth']);
-        $this->assertEquals('+123456789', $aResult['phone']);
-        $this->assertEquals('Street 1', $aResult['address']['street1']);
-        $this->assertEquals('City', $aResult['address']['city']);
-        $this->assertEquals('CC', $aResult['address']['country']);
-        $this->assertEquals('State', $aResult['address']['state']);
-        $this->assertEquals('1234', $aResult['address']['postal-code']);
-        $this->assertEquals('f', $aResult['gender']);
+        $aExpected = [
+            'last-name' => 'LastName',
+            'first-name' => 'FirstName',
+            'email' => 'test@test.com',
+            'date-of-birth' => '02-09-1979',
+            'phone' => '+123456789',
+            'address' => [
+                'street1' => 'Street 1',
+                'city' => 'City',
+                'country' => 'CC',
+                'state' => 'State',
+                'postal-code' => '1234',
+            ],
+            'gender' => 'f',
+        ];
+
+        $this->assertEquals($aExpected, $aResult);
     }
 
     public function testCreateAccountHolderWithMandatoryDataSet()
@@ -71,13 +72,17 @@ class AccountHolderHelperTest extends OxidEsales\TestingLibrary\UnitTestCase
         $oAccountHolder = $this->_oAccountHolderHelper->createAccountHolder($aArgs);
         $aResult = $oAccountHolder->mappedProperties();
 
-        $this->assertCount(4, $aResult);
-        $this->assertCount(3, $aResult['address']);
-        $this->assertEquals('LastName', $aResult['last-name']);
-        $this->assertEquals('FirstName', $aResult['first-name']);
-        $this->assertEquals('test@test.com', $aResult['email']);
-        $this->assertEmpty($aResult['address']['street1']);
-        $this->assertEmpty($aResult['address']['city']);
-        $this->assertEmpty($aResult['address']['country']);
+        $aExpected = [
+            'last-name' => 'LastName',
+            'first-name' => 'FirstName',
+            'email' => 'test@test.com',
+            'address' => [
+                'street1' => '',
+                'city' => '',
+                'country' => '',
+            ],
+        ];
+
+        $this->assertEquals($aExpected, $aResult);
     }
 }
