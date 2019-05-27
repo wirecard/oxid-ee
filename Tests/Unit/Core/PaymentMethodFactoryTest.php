50cf2ddfe006a33c5a24dc1298da9a7cca9b0b65
Christian Grach <grach@parkside.at>
2019-05-27 14:59:44 +0200

    update PaymentMethodFactoryTest and PaymentMethodTest

--- Tests/Unit/Core/PaymentMethodFactoryTest.php
+++ Tests/Unit/Core/PaymentMethodFactoryTest.php
@@ -8,6 +8,12 @@
  */
 
 use Wirecard\Oxid\Core\PaymentMethodFactory;
+use Wirecard\Oxid\Model\CreditCardPaymentMethod;
+use Wirecard\Oxid\Model\PayolutionInvoicePaymentMethod;
+use Wirecard\Oxid\Model\PaypalPaymentMethod;
+use Wirecard\Oxid\Model\RatepayInvoicePaymentMethod;
+use Wirecard\Oxid\Model\SepaCreditTransferPaymentMethod;
+use Wirecard\Oxid\Model\SepaDirectDebitPaymentMethod;
 
 use Wirecard\Oxid\Model\PaymentMethod;
 
@@ -20,27 +26,47 @@ class PaymentMethodFactoryTest extends OxidEsales\TestingLibrary\UnitTestCase
         }
     }
 
+    /**
+     * @expectedException \OxidEsales\Eshop\Core\Exception\StandardException
+     */
+    public function testInvalidPaymentMethod()
+    {
+        PaymentMethodFactory::create('invalid');
+    }
+
     public function testCreatePaypal()
     {
         $oPaymentMethod = PaymentMethodFactory::create("wdpaypal");
-        $this->assertInstanceOf(\Wirecard\Oxid\Model\PaypalPaymentMethod::class, $oPaymentMethod);
+        $this->assertInstanceOf(PaypalPaymentMethod::class, $oPaymentMethod);
     }
 
     public function testCreateCreditCard()
     {
         $oPaymentMethod = PaymentMethodFactory::create("wdcreditcard");
-        $this->assertInstanceOf(\Wirecard\Oxid\Model\CreditCardPaymentMethod::class, $oPaymentMethod);
+        $this->assertInstanceOf(CreditCardPaymentMethod::class, $oPaymentMethod);
     }
 
     public function testCreateSepaCreditTransfer()
     {
         $oPaymentMethod = PaymentMethodFactory::create("wdsepacredit");
-        $this->assertInstanceOf(\Wirecard\Oxid\Model\SepaCreditTransferPaymentMethod::class, $oPaymentMethod);
+        $this->assertInstanceOf(SepaCreditTransferPaymentMethod::class, $oPaymentMethod);
     }
 
     public function testCreateSepaDirectDebit()
     {
         $oPaymentMethod = PaymentMethodFactory::create("wdsepadd");
-        $this->assertInstanceOf(\Wirecard\Oxid\Model\SepaDirectDebitPaymentMethod::class, $oPaymentMethod);
+        $this->assertInstanceOf(SepaDirectDebitPaymentMethod::class, $oPaymentMethod);
+    }
+
+    public function testCreatePayolutionInvoice()
+    {
+        $oPaymentMethod = PaymentMethodFactory::create("wdpayolution-inv");
+        $this->assertInstanceOf(PayolutionInvoicePaymentMethod::class, $oPaymentMethod);
+    }
+
+    public function testCreateRatepayInvoice()
+    {
+        $oPaymentMethod = PaymentMethodFactory::create("wdratepay-invoice");
+        $this->assertInstanceOf(RatepayInvoicePaymentMethod::class, $oPaymentMethod);
     }
 }
