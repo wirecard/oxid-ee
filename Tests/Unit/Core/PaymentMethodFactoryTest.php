7937477b6467de175f9b372954682125b9892cda
Martin Holler <holler@parkside.at>
2019-06-06 16:15:44 +0200

    Update factory test

--- Tests/Unit/Core/PaymentMethodFactoryTest.php
+++ Tests/Unit/Core/PaymentMethodFactoryTest.php
@@ -29,10 +29,9 @@ class PaymentMethodFactoryTest extends OxidEsales\TestingLibrary\UnitTestCase
     /**
      * @dataProvider createProvider
      */
-    public function testCreatePaypal()
+    public function testCreate($sPaymentMethodType, $oExpectedClassName)
     {
-        $oPaymentMethod = PaymentMethodFactory::create("wdpaypal");
-        $this->assertInstanceOf(PaypalPaymentMethod::class, $oPaymentMethod);
+        $this->assertInstanceOf($oExpectedClassName, PaymentMethodFactory::create($sPaymentMethodType));
     }
 
     public function createProvider()
