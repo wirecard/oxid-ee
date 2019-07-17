6350c788adfee91799ff380c75f86bfe5e7f0ebf
Ajdin Mehic <mehic@parkside.at>
2019-07-17 14:23:28 +0200

    Remove unused class

--- Controller/Admin/Transaction/TransactionTabPostProcessing.php
+++ Controller/Admin/Transaction/TransactionTabPostProcessing.php
@@ -19,7 +19,6 @@ use Wirecard\Oxid\Core\PaymentMethodFactory;
 use Wirecard\Oxid\Core\PostProcessingHelper;
 use Wirecard\Oxid\Core\TransactionHandler;
 use Wirecard\Oxid\Model\Transaction;
-use Wirecard\Oxid\Model\PaymentMethod\PaymentMethod;
 
 use Wirecard\PaymentSdk\BackendService;
 use Wirecard\PaymentSdk\Config\Config;
