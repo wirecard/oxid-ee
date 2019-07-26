2528fe9a06a340d16c5c73b0d4b1e07788ab1452
Michael Stifter <stifter@parkside.at>
2019-07-26 11:56:26 +0200

    Add test for getting translated transaction state

--- Tests/Integration/Model/TransactionTest.php
+++ Tests/Integration/Model/TransactionTest.php
@@ -7,9 +7,11 @@
  * https://github.com/wirecard/oxid-ee/blob/master/LICENSE
  */
 
-use Wirecard\Oxid\Model\Transaction;
-
 use OxidEsales\Eshop\Application\Model\Order;
+
+use Wirecard\Oxid\Model\PaymentMethod\BasePoiPiaPaymentMethod;
+use Wirecard\Oxid\Core\PaymentMethodHelper;
+use Wirecard\Oxid\Model\Transaction;
 
 class TransactionTest extends Wirecard\Test\WdUnitTestCase
 {
@@ -17,19 +19,36 @@ class TransactionTest extends Wirecard\Test\WdUnitTestCase
     {
         return [
             [
+                'table' => 'oxorder',
+                'columns' => [
+                    'oxid',
+                    'oxpaymenttype',
+                ],
+                'rows' => [
+                    ['order 1', 'wdpaymentinadvance'],
+                    ['order 2', 'wdpaymentoninvoice'],
+                    ['order 3', 'wdcreditcard'],
+                ],
+            ],
+            [
                 'table' => 'wdoxidee_ordertransactions',
                 'columns' => [
                     'oxid',
+                    'orderid',
                     'transactionid',
                     'parenttransactionid',
                     'responsexml',
+                    'state',
                 ],
                 'rows' => [
-                    ['1', '1', null, base64_encode('<?xml version="1.0" encoding="UTF-8"?>')],
-                    ['2', '1.1', '1', ''],
-                    ['3', '1.2', '1', ''],
-                    ['4', '2', null, ''],
-                    ['5', '3', null, ''],
+                    ['1', 'order 1', '1', null, base64_encode('<?xml version="1.0" encoding="UTF-8"?>'), 'success'],
+                    ['2', 'order 1', '1.1', '1', '', 'success'],
+                    ['3', 'order 1', '1.2', '1', '', 'success'],
+                    ['4', 'order 1', '2', null, '', 'success'],
+                    ['5', 'order 1', '3', null, '', 'success'],
+                    ['6', 'order 1', 't4', null, '', 'success'],
+                    ['7', 'order 2', 't5', null, '', 'awaiting'],
+                    ['8', 'order 3', 't6', null, '', 'xxx'],
                 ],
             ],
         ];
@@ -82,5 +101,24 @@ class TransactionTest extends Wirecard\Test\WdUnitTestCase
         $oTransaction = oxNew(Transaction::class);
         $aTransactionStates = $oTransaction->getTranslatedStates();
         $this->assertCount(4, $aTransactionStates);
+    }
+
+    /**
+     * @dataProvider getTranslatedStateProvider
+     */
+    public function testGetTranslatedState($sExpected, $sTransactionId)
+    {
+        $oTransaction = oxNew(Transaction::class);
+        $oTransaction->loadWithTransactionId($sTransactionId);
+        $this->assertEquals($sExpected, $oTransaction->getTranslatedState());
+    }
+
+    public function getTranslatedStateProvider()
+    {
+        return [
+            'translated success' => ['erfolgreich', 't4'],
+            'translated awaiting' => ['ausstehend', 't5'],
+            'no translation found' => ['', 't6'],
+        ];
     }
 }
